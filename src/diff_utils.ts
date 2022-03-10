cc9ba1f92093f806d0bd4f7530e059271849c1e1
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-10 21:55:03 +0100

    feat: create helper functions

--- src/diff_utils.ts
+++ src/diff_utils.ts
@@ -1,10 +1,26 @@
-import type { Plugin, App} from 'obsidian';
-import type OpenSyncHistoryPlugin from './main'
+import type { Plugin, App, TFile } from 'obsidian';
+import type OpenSyncHistoryPlugin from './main';
+import type { gHResult, item, syncInstance } from './interfaces';
 
 export default class DiffUtils {
+	instance: syncInstance;
 
 	constructor(private plugin: OpenSyncHistoryPlugin, private app: App) {
 		this.plugin = plugin;
-		this.app = app
+		this.app = app;
+		this.instance = this.app.internalPlugins.plugins.sync.instance;
+	}
+
+	async getVersions(
+		file: TFile,
+		uid: number | null = null
+	): Promise<gHResult> {
+		return await this.instance.getHistory(file.path, uid);
+	}
+
+	async getContent(uid: number) {
+		const content = await this.instance.getContentForVersion(uid);
+		const textDecoder = new TextDecoder('utf-8');
+		const text = textDecoder.decode(new Uint8Array(content));
 	}
 }
