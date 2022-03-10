0df8eb2ce00f73dd8dcc27e5110cb870c190bd64
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-10 23:36:41 +0100

    feat: add unified diff child process method

--- src/diff_utils.ts
+++ src/diff_utils.ts
@@ -1,6 +1,7 @@
 import type { Plugin, App, TFile } from 'obsidian';
 import type OpenSyncHistoryPlugin from './main';
 import type { gHResult, item, syncInstance } from './interfaces';
+import { spawn } from 'child_process';
 
 export default class DiffUtils {
 	instance: syncInstance;
@@ -22,5 +23,38 @@ export default class DiffUtils {
 		const content = await this.instance.getContentForVersion(uid);
 		const textDecoder = new TextDecoder('utf-8');
 		const text = textDecoder.decode(new Uint8Array(content));
+	}
+
+	// Thank you: https://github.com/marcusolsson/obsidian-vale/blob/fdc0fc5d1c259cc823b867ae2d278f09703acf43/src/vale/ValeCli.ts#L12
+	async getUnifiedDiff(str1: string, str2: string): Promise<string | null> {
+		const child = spawn('diff', [
+			'-u',
+			`<(echo '${str1}')`,
+			`<(echo '${str2}')`,
+		]);
+
+		let stdout = '';
+
+		if (child.stdout) {
+			child.stdout.on('data', (data) => {
+				stdout += data;
+			});
+		}
+		return new Promise((resolve, reject) => {
+			child.on('error', reject);
+
+			child.on('close', (code) => {
+				if (code === 0) {
+					// diff exited without errors
+					resolve(stdout);
+				} else if (code === 1) {
+					// diff returned alerts.
+					resolve(null);
+				} else {
+					// diff exited unexpectedly.
+					reject(new Error(`child exited with code ${code}`));
+				}
+			});
+		});
 	}
 }
