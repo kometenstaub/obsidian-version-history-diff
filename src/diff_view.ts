279f1dee34015a234367cabf583b07c6ee8541a3
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-10 23:57:06 +0100

    feat: flesh out createHtml with dummy data

--- src/diff_view.ts
+++ src/diff_view.ts
@@ -1,8 +1,10 @@
 import { html } from 'diff2html';
-import { App, Modal, TFile } from 'obsidian';
+import { App, Modal, Notice, TFile } from 'obsidian';
 import type OpenSyncHistoryPlugin from './main';
 
 export default class DiffView extends Modal {
+	parser: DOMParser;
+
 	constructor(
 		private plugin: OpenSyncHistoryPlugin,
 		public app: App,
@@ -12,12 +14,25 @@ export default class DiffView extends Modal {
 		this.plugin = plugin;
 		this.app = app;
 		this.file = file;
+		this.parser = new DOMParser()
 		this.createHtml();
 	}
 
-	createHtml() {
+	async createHtml() {
 		const versions = this.plugin.diff_utils.getVersions(this.file);
+		// need to choose the two versions somehow
+		const [v1, v2] = [10, 20] // dummy versions
+		const { getContent, getUnifiedDiff } = this.plugin.diff_utils
+		const [c1, c2] = [await getContent(v1), await getContent(v2)]
 		this.titleEl.setText('Diff view');
-		//this.contentEl.
+		const uDiff = await getUnifiedDiff(c1, c2)
+		if (uDiff !== null) {
+			const diff = html(uDiff)
+			const parsedHtml = this.parser.parseFromString(diff, 'text/html')
+			this.contentEl.append(parsedHtml)
+		} else {
+			new Notice('Something went wrong.')
+		}
+
 	}
 }
