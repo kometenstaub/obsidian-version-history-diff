9954e04c9f5425d7ecca9f1096ea1784ad733c9a
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-10 21:54:39 +0100

    refactor: extract interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -5,28 +5,34 @@ declare module 'obsidian' {
 		internalPlugins: {
 			plugins: {
 				sync: {
-					instance: {
-						getHistory(
-							path: string,
-							// use the UID of the last element in the array to get the next 30 versions
-							uid?: null | number
-						): Promise<{
-							items:
-								{
-									deleted: boolean;
-									device: string; // from which device that version is
-									folder: boolean;
-									path: string;
-									size: number;
-									ts: number; // `new Date(ts)` outputs the date
-									uid: number; // unique UID for the whole vault
-								}[];
-							more: boolean; // whether there are more versions
-						}>;
-						showVersionHistory(path: string): Promise<void>;
-					};
+					instance: syncInstance;
 				};
 			};
 		};
 	}
+}
+
+export interface syncInstance {
+	getHistory(
+		path: string,
+		// use the UID of the last element in the array to get the next 30 versions
+		uid?: null | number
+	): Promise<gHResult>;
+	showVersionHistory(path: string): Promise<void>;
+	getContentForVersion(uid: number): Promise<ArrayBuffer>;
+}
+
+export interface gHResult {
+	items: item[];
+	more: boolean; // whether there are more versions
+}
+
+export interface item {
+	deleted: boolean;
+	device: string; // from which device that version is
+	folder: boolean;
+	path: string;
+	size: number;
+	ts: number; // `new Date(ts)` outputs the date
+	uid: number; // unique UID for the whole vault
 }
