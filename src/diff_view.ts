acb0558f8930769cce06612487d850edd6d1e2b0
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-11 18:02:39 +0100

    feat: start modal logic

--- src/diff_view.ts
+++ src/diff_view.ts
@@ -1,5 +1,6 @@
 import { html } from 'diff2html';
 import { App, Modal, Notice, TFile } from 'obsidian';
+import type { gHResult } from './interfaces';
 import type OpenSyncHistoryPlugin from './main';
 
 export default class DiffView extends Modal {
@@ -15,26 +16,69 @@ export default class DiffView extends Modal {
 		this.app = app;
 		this.file = file;
 		this.parser = new DOMParser();
+		this.modalEl.addClasses(['mod-sync-history', 'diff'])
 		this.createHtml();
 	}
 
 	async createHtml() {
+		// get first thirty versions
 		const versions = await this.plugin.diff_utils.getVersions(this.file);
+		// for initial display
+		let [latestV, secondLatestV] = [0, 0]
+		if (versions.items.length > 1) {
+			latestV = versions.items[0].uid
+			secondLatestV = versions.items[1].uid
+		} else {
+			new Notice('There are not at least two versions.')
+			this.close()
+		}
+
+		// get functions
 		const getContent = this.plugin.diff_utils.getContent.bind(this);
 		const getUnifiedDiff = this.plugin.diff_utils.getUnifiedDiff;
+
 		// need to choose the two versions somehow
-		const [v1, v2] = [2037, 2009]; // dummy versions; iOS app file
-		const [c1, c2] = [await getContent(v1), await getContent(v2)];
-		//console.log(c1, '\n\n' ,c2)
+		const [c1, c2] = [await getContent(secondLatestV), await getContent(latestV)];
+
 		this.titleEl.setText('Diff view');
 		const uDiff = await getUnifiedDiff(c1, c2);
-		//console.log(`Udiff: ${uDiff}`)
+
 		if (typeof uDiff === 'string') {
 			const diff = html(uDiff, {outputFormat: 'side-by-side'});
 			const parsedHtml = this.parser.parseFromString(diff, 'text/html');
-			this.contentEl.appendChild(parsedHtml.documentElement);
+
+			const leftHistory = this.createHistory(this.contentEl)
+			const rightHistory = this.createHistory(this.contentEl)
+			const syncHistoryContentContainer = parsedHtml.documentElement
+			syncHistoryContentContainer.addClasses(['sync-history-content-container', 'diff'])
+
+			this.contentEl.appendChild(leftHistory)
+			this.contentEl.appendChild(syncHistoryContentContainer);
+			this.contentEl.appendChild(rightHistory)
+
+
+
 		} else {
 			new Notice('Something went wrong.');
+		}
+	}
+
+	createHistory(el: HTMLElement): HTMLElement {
+		const syncHistoryListContainer = el.createDiv({
+			cls: 'sync-history-list-container'
+		})
+		const syncHistoryList = syncHistoryListContainer.createDiv({
+			cls: 'sync-history-list'
+		})
+		return syncHistoryListContainer
+	}
+
+	appendVersions(el: HTMLElement, versions: gHResult): void {
+		for (let version of versions.items) {
+			const div = el.createDiv({
+				cls: 'sync-history-list-item',
+				text: new Date(version.ts).toDateString()
+			})
 		}
 	}
 }
