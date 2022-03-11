594cb601350a8a022ec9c72a8c50a7125728af14
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-11 12:02:44 +0100

    fix: referencing and getUnifiedDiff()

--- src/diff_utils.ts
+++ src/diff_utils.ts
@@ -1,61 +1,49 @@
 import type { Plugin, App, TFile } from 'obsidian';
 import type OpenSyncHistoryPlugin from './main';
 import type { gHResult, item, syncInstance } from './interfaces';
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 
 export default class DiffUtils {
+	plugin: OpenSyncHistoryPlugin;
+	app: App;
 	instance: syncInstance;
 
-	constructor(private plugin: OpenSyncHistoryPlugin, private app: App) {
+	constructor(plugin: OpenSyncHistoryPlugin, app: App) {
 		this.plugin = plugin;
 		this.app = app;
-		this.instance = this.app.internalPlugins.plugins.sync.instance;
+		this.instance = app.internalPlugins.plugins.sync.instance
 	}
 
 	async getVersions(
 		file: TFile,
 		uid: number | null = null
 	): Promise<gHResult> {
+		//const { instance } = this.app.internalPlugins.plugins.sync
 		return await this.instance.getHistory(file.path, uid);
 	}
 
 	async getContent(uid: number): Promise<string> {
-		const content = await this.instance.getContentForVersion(uid);
+		const content = await this.app.internalPlugins.plugins.sync.instance.getContentForVersion(uid);
 		const textDecoder = new TextDecoder('utf-8');
 		const text = textDecoder.decode(new Uint8Array(content));
-		return text
+		return text;
 	}
 
 	// Thank you: https://github.com/marcusolsson/obsidian-vale/blob/fdc0fc5d1c259cc823b867ae2d278f09703acf43/src/vale/ValeCli.ts#L12
-	async getUnifiedDiff(str1: string, str2: string): Promise<string | null> {
-		const child = spawn('diff', [
-			'-u',
-			`<(echo '${str1}')`,
-			`<(echo '${str2}')`,
-		]);
-
-		let stdout = '';
-
-		if (child.stdout) {
-			child.stdout.on('data', (data) => {
-				stdout += data;
-			});
-		}
-		return new Promise((resolve, reject) => {
-			child.on('error', reject);
-
-			child.on('close', (code) => {
-				if (code === 0) {
-					// diff exited without errors
-					resolve(stdout);
-				} else if (code === 1) {
-					// diff returned alerts.
-					resolve(null);
-				} else {
-					// diff exited unexpectedly.
-					reject(new Error(`child exited with code ${code}`));
-				}
-			});
+	async getUnifiedDiff(str1: string, str2: string): Promise<void> {
+		const child = exec(`diff -u <(echo "${str1}") <(echo "${str2}")`, (error, stdout, stderr) => {
+			if (stdout) {
+				console.log(stdout)
+				return
+			}
+			else if (error) {
+				console.error(`exec error: ${error}`)
+				return
+			}
+			else if (stderr) {
+				console.error(`stderr: ${stderr}`)
+			}
 		});
+
 	}
 }
