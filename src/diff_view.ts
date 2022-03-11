a18ba7b505a6ed6f401028443a855b1b766bc43e
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-11 12:50:29 +0100

    fix: small changes, but still broken

--- src/diff_view.ts
+++ src/diff_view.ts
@@ -14,25 +14,27 @@ export default class DiffView extends Modal {
 		this.plugin = plugin;
 		this.app = app;
 		this.file = file;
-		this.parser = new DOMParser()
+		this.parser = new DOMParser();
 		this.createHtml();
 	}
 
 	async createHtml() {
-		const versions = this.plugin.diff_utils.getVersions(this.file);
+		const versions = await this.plugin.diff_utils.getVersions(this.file)
+		const getContent = this.plugin.diff_utils.getContent.bind(this);
+		const getUnifiedDiff = this.plugin.diff_utils.getUnifiedDiff;
 		// need to choose the two versions somehow
-		const [v1, v2] = [10, 20] // dummy versions
-		const { getContent, getUnifiedDiff } = this.plugin.diff_utils
-		const [c1, c2] = [await getContent(v1), await getContent(v2)]
+		const [v1, v2] = [2037, 2009]; // dummy versions; iOS app file
+		const [c1, c2] = [await getContent(v1), await getContent(v2)];
+		//console.log(c1, '\n\n' ,c2)
 		this.titleEl.setText('Diff view');
-		const uDiff = await getUnifiedDiff(c1, c2)
-		if (uDiff !== null) {
-			const diff = html(uDiff)
-			const parsedHtml = this.parser.parseFromString(diff, 'text/html')
-			this.contentEl.append(parsedHtml)
+		const uDiff = await getUnifiedDiff(c1, c2);
+		//console.log(`Udiff: ${uDiff}`)
+		if (typeof uDiff === 'string') {
+			const diff = html(uDiff);
+			const parsedHtml = this.parser.parseFromString(diff, 'text/html');
+			this.contentEl.append(parsedHtml);
 		} else {
-			new Notice('Something went wrong.')
+			new Notice('Something went wrong.');
 		}
-
 	}
 }
