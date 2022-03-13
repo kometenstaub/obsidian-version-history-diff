55984ea6bef1b6b63eee07e8ed5014f561466e00
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-13 17:03:35 +0100

    feat: restore and switch reading/raw mode buttons

--- src/file_modal.ts
+++ src/file_modal.ts
@@ -1,27 +1,66 @@
-import {App, MarkdownRenderer, Modal, TFile } from "obsidian";
+import {App, MarkdownRenderer, Modal, Notice, sanitizeHTMLToDom, TFile } from "obsidian";
 import type OpenSyncHistoryPlugin from './main'
 
 export default class FileModal extends Modal {
+	raw: boolean;
+
 	constructor(private plugin: OpenSyncHistoryPlugin, public app: App, private syncFile: string, private file: TFile) {
 		super(app);
 		this.plugin = plugin
 		this.app = app
 		this.file = file
 		this.syncFile = syncFile
+		this.raw = false
 
 	}
 
 	async onOpen() {
 		super.onOpen()
-		const el = this.contentEl;
+
+		const restoreButton = this.contentEl.createEl('button', {
+			cls: [ 'mod-cta', 'restore' ],
+			text: `Replace ${this.file.basename} content with this version`
+		})
+		const switchButton = this.contentEl.createEl('button', {
+			cls: [ 'mod-cta', 'switch'],
+			text: 'Show raw text'
+		})
+
+		const el = this.contentEl.createDiv();
+
+		switchButton.addEventListener('click', () => {
+			if (!this.raw) {
+				const lines = this.syncFile.split('\n')
+				let linesWithBrs = '<br />'
+				for (const line of lines) {
+					linesWithBrs += line
+					linesWithBrs += '<br />'
+				}
+				//const html = sanitizeHTMLToDom(BrString)
+				el.empty()
+				el.innerHTML = linesWithBrs
+				//el.innerHTML = this.syncFile
+				this.raw = !this.raw
+				switchButton.innerText = 'Show Reading view'
+			} else {
+				this.raw = !this.raw;
+				(async () => {
+					el.empty()
+					await MarkdownRenderer.renderMarkdown(this.syncFile, el, this.file.path, null)
+				})();
+				switchButton.innerText = 'Show raw text'
+			}
+		})
+
+		restoreButton.addEventListener('click', () => {
+			(async () => {
+				await this.app.vault.modify(this.file, this.syncFile)
+			})();
+			new Notice(`The file ${this.file.basename} file has been overwritten with the selected version.`)
+			this.close()
+		})
 
 		await MarkdownRenderer.renderMarkdown(this.syncFile, el, null, null)
-		// maybe render the current version on disk right to it, but that won't highlight differences
-
-		//const markdown = await this.app.vault.read(file)
-		//await MarkdownRenderer.renderMarkdown(markdown, el, null, null)
-
-		// render button that will replace the current file with what is shown at the moment
 	}
 
 }
