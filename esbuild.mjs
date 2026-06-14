7dedc7ad923ee148c42e662bbb1b2e17d300bdc6
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2026-06-14 20:54:35 +0200

    build: update dependencies

--- esbuild.mjs
+++ esbuild.mjs
@@ -92,8 +92,8 @@ https://github.com/kpdecker/jsdiff
 	OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	
 	
-diff2html uses hogan.js:
-https://github.com/twitter/hogan.js
+diff2html uses @profoundlogic/hogan:
+https://github.com/ProfoundLogic/hogan.js-ProfoundLogic
 
                                  Apache License
                            Version 2.0, January 2004
@@ -272,7 +272,7 @@ https://github.com/twitter/hogan.js
 
    END OF TERMS AND CONDITIONS
 
-hogan.js uses nopt:
+@profoundlogic/hogan uses nopt:
 https://github.com/npm/nopt
 
 	The ISC License
@@ -316,31 +316,6 @@ https://github.com/npm/abbrev-js
 	IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 	
 
-hogan.js uses mkdirp:
-https://github.com/isaacs/node-mkdirp
-
-	Copyright (c) 2011-2022 James Halliday (mail@substack.net) and Isaac Z. Schlueter (i@izs.me)
-	
-	This project is free software released under the MIT license:
-	
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-	
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
-	
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	THE SOFTWARE.
-	
 */
 `;
 
